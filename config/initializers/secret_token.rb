c8cc58448cc654f6ace3326063a3a61d8d3bf9ff
Matt Moriarity <matt@mattmoriarity.com>
2013-07-02 20:34:54 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmealsWeb::Application.config.secret_key_base = '975327cc9d393a0c65f709e414dcbca9a441c37b06ccef5eee11d53849329995d4f8d5c4a8bdcbde4ebd171f03d9ec135ccbcec732551f205cb81ac75e8a6b3e'
